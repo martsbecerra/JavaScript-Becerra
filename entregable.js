07b2cb3e3e54b6106fdea8448f6a9d53168b2fe5
Martin Becerra <martinn.becerra@gmail.com>
2022-06-22 19:31:17 -0300

    primer desafio complementario

---
+++ entregable.js
@@ -0,0 +1,14 @@
+//algoritmo para identificar cuantos pisos debe subir una persona para llegar a la terraza
+
+let piso = parseInt(prompt("ingrese piso en el que vive"))
+let totalPisos = parseInt(prompt("ingrese cantidad de pisos de su edificio"))
+let subirPisos = 0
+
+while(piso >= 0 && piso <= totalPisos){
+    subirPisos = subirPisos + 1
+    piso = piso + 1
+    console.log(subirPisos)
+}
+
+alert(`debe subir ${subirPisos} hasta llegar a la terraza`)
+
